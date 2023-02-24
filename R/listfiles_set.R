b5cf76984d0834cd48f8003c34c0ef7bbd9c36e7
Ernie Pedapati <ernest.pedapati@cchmc.org>
2023-02-24 10:08:32 -0500

    updated build

--- R/listfiles_set.R
+++ R/listfiles_set.R
@@ -3,19 +3,26 @@
 #' Find all SET files in a directory tree containing a certain string, and return the results as a # nolint
 #' data frame.
 #'
+#' @import dplyr
+#' @import magrittr
+#' @import tibble
+#' @import tidyr
+#' @importFrom rlang .data
 #' @param dir_path A character string specifying the root directory to search.
 #' @param subdir_string A character string specifying the subdirectory pattern to search for.
 #' @param custom_keyword A character string specifying the keyword to search for in the SET file names.
 #' @return A data frame with the results, including the subdirectory, file name (without extension), and extension.
+#' @name listfiles_set
+#' @keywords listfiles_set, files, directory
 #' @examples
 #' # List SET files in a directory tree containing "data" and "subject1"
 #' # listfiles_set("/path/to/directory", "data|subject1", "SET")
 
-library(tidyverse)
 
 listfiles_set <- function(dir_path = ".", subdir_string = "", custom_keyword = "") {
   # Find all files in a directory tree with a given extension and containing a keyword
   # in their name, in subdirectories containing a certain string
+#  requireNamespace("dplyr", quietly = TRUE)
 
   # Find subdirectories containing a certain string
   subdir_paths <- list.files(dir_path, recursive = TRUE, full.names = TRUE, include.dirs = TRUE)
@@ -35,7 +42,6 @@ listfiles_set <- function(dir_path = ".", subdir_string = "", custom_keyword = "
   }
 
   # Generate a data frame with the results
-  library(tidyverse)
   file_df <- tibble(file_paths) %>%
     # Extract the subdirectory and filename (without extension) from the full file path
     mutate(subdir = dirname(file_paths),
